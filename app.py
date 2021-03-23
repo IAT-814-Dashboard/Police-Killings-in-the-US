46c04eb284da9e0391ab558e86bfcc2669607cd2
saptarshidg <saptarshiduttagupta@gmail.com>
2021-03-22 20:42:11 -0700

    Update app.py

--- app.py
+++ app.py
@@ -21,95 +21,92 @@ styles = {
 
 df = pd.read_json('police-killings-integrated-dataset-2021-03-20.json.gz')
 
+viz_states = {'pie_chart':0 , 'choropleth_map':0, 'line_chart':0, 'stacked_bar_chart':0 }
+
 #Pie chart
-df_group_by_race = df.groupby('race')['name'].agg('count').reset_index().rename(columns={'name':'count'})
-pie_chart = go.Figure(data=[go.Pie(labels=df_group_by_race['race'], values=df_group_by_race['count'], title='Ethnicity Versus Killings', textinfo='label+percent',
+def create_pie_chart(df):
+    df_group_by_race = df.groupby('race')['name'].agg('count').reset_index().rename(columns={'name':'count'})
+    pie_chart = go.Figure(data=[go.Pie(labels=df_group_by_race['race'], values=df_group_by_race['count'], title='Ethnicity Versus Killings', textinfo='label+percent',
                              insidetextorientation='radial')])
+    return pie_chart
 
 #Choropleth map
-df_group_by_state = df.groupby('state')['name'].agg('count').reset_index().rename(columns={'name':'count'})
-choropleth_map = go.Figure(data=go.Choropleth(
-    locations=df_group_by_state['state'],
-    z = df_group_by_state['count'],
-    locationmode = 'USA-states',
-    colorscale = 'Blues',
-    colorbar_title = "Number of Deaths"
-))
-choropleth_map.update_layout(
-    title_text = 'Police Shooting Deaths by US States',
-    geo_scope='usa'
-)
-choropleth_map.update_layout(clickmode='event+select')
+def create_choropleth_map(df):
+    df_group_by_state = df.groupby('state')['name'].agg('count').reset_index().rename(columns={'name':'count'})
+    choropleth_map = go.Figure(data=go.Choropleth(
+        locations=df_group_by_state['state'],
+        z = df_group_by_state['count'],
+        locationmode = 'USA-states',
+        colorscale = 'Blues',
+        colorbar_title = "Number of Deaths"
+    ))
+    choropleth_map.update_layout(
+        title_text = 'Police Shooting Deaths by US States',
+        geo_scope='usa'
+    )
+    return choropleth_map
+#choropleth_map.update_layout(clickmode='event+select')
 
 #stacked bar chart
-df_group_by_age_gender = df.groupby(['age_bins','gender'])['name'].agg('count').reset_index().rename(columns={'name':'count'})
-stacked_bar = px.bar(df_group_by_age_gender, x="age_bins", y="count", color="gender",
+def create_stacked_bar_chart(df):
+    df_group_by_age_gender = df.groupby(['age_bins','gender'])['name'].agg('count').reset_index().rename(columns={'name':'count'})
+    stacked_bar = px.bar(df_group_by_age_gender, x="age_bins", y="count", color="gender",
             hover_data=['count'], barmode = 'stack', custom_data=['gender'])
+    return stacked_bar
 
- 
+
 #line chart
-df_group_by_date = df.groupby('date')['name'].agg('count').reset_index().rename(columns={'name':'count'})
-line_chart = px.line(df_group_by_date, x='date', y="count")
+def create_line_chart(df):
+    df_group_by_date = df.groupby('date')['name'].agg('count').reset_index().rename(columns={'name':'count'})
+    line_chart = px.line(df_group_by_date, x='date', y="count")
+    return line_chart
 
+def get_age_and_gender(stackBarClick):
+    gender_id = stackBarClick['points'][0]['curveNumber']
+    if gender_id==0:
+        gender='Female'
+    else:
+        gender='Male'
+
+    age = stackBarClick['points'][0]['x']
+    return gender, age
 
 #-----------------------------------------------------------------------------------------------------------------------------------
 
 app.layout = html.Div([
-    
+
     #html.Div(id='test-output'),
-    
-    dcc.Graph(
+
+    dcc.Loading(dcc.Graph(
                 id='line-chart',
-                figure=line_chart
-    ),
-    
-    html.Div([
-            dcc.Graph(
-                        id='pie-chart-interaction',
-                        figure=pie_chart,
-                        config={#'responsive': True,
-                                'doubleClick':'reset'},
-            ),
-            
-            dcc.Graph(
-                        id='stacked-bar-chart',
-                        figure=stacked_bar,
-                        #config={#'responsive': True,
-                        #        'doubleClick':'reset'},
-            ),
-            
-    ], style={'columnCount': 2}),
-            
-    dcc.Graph(
+                figure=create_line_chart(df)
+    )),
+
+    dcc.Loading(dcc.Loading(dcc.Graph(
                 id='choropleth-map',
-                figure=choropleth_map,
+                figure=create_choropleth_map(df),
                 #config={#'responsive': True,
                 #                'doubleClick':'reset'},
-    ),
+    ))),
+    html.Div([
+            dcc.Loading(dcc.Graph(
+                        id='pie-chart-interaction',
+                        figure=create_pie_chart(df),
+                        config={#'responsive': True,
+                                'doubleClick':'reset'},
+            )),
+
+            dcc.Loading(dcc.Graph(
+                        id='stacked-bar-chart',
+                        figure=create_stacked_bar_chart(df),
+            )),
+
+    ], style={'columnCount': 2}),
+
 ])
 
 #-----------------------------------------------------------------------------------------------------------------------------------
 
-"""
-@app.callback(
-    Output('test-output','children'),
-    [Input('pie-chart-interaction', 'clickData'),
-    Input('stacked-bar-chart', 'clickData'),
-    Input('line-chart','relayoutData'),
-    Input('choropleth-map','clickData')], prevent_initial_call=True)
-def update_text_box(pieClick, stackBarClick, lineChartClick, mapClick):
-    if pieClick:
-        return "Race:{}".format(pieClick['points'][0]['label'])
-    if stackBarClick:
-        gender_id = stackBarClick['points'][0]['curveNumber']
-        if gender_id==0:
-            gender='Female'
-        else:
-            gender='Male'
-            
-        age = stackBarClick['points'][0]['x'] 
-        return "Gender={}\nAge={}".format(gender, age)
-"""
 
 @app.callback(
     Output('choropleth-map', 'figure'),
@@ -117,117 +114,51 @@ def update_text_box(pieClick, stackBarClick, lineChartClick, mapClick):
     Input('stacked-bar-chart', 'clickData'),
     Input('line-chart','relayoutData')], prevent_initial_call=True)
 def update_choropleth_map(pieClick, stackBarClick, lineChartClick):
-    print('-------------------------------------')
-    print("Inside update choropleth function")
-    print("Pie click:",pieClick)
-    print("Stacked Bar Click:",stackBarClick)
-    print("Line chart click:",lineChartClick)
-    
-    if pieClick:
+    if viz_states['choropleth_map']==1:
+        return dash.no_update
+    triggered_element = dash.callback_context.triggered[0]['prop_id'].split('.')[0]
+    if triggered_element =='pie-chart-interaction':
         filter_by_race = df[df['race']==str(pieClick['points'][0]['label'])]
-        df_group_by_state = filter_by_race.groupby('state')['name'].agg('count').reset_index().rename(columns={'name':'count'})
-        choropleth_map = go.Figure(data=go.Choropleth(
-        locations=df_group_by_state['state'],
-        z = df_group_by_state['count'],
-        locationmode = 'USA-states',
-        colorscale = 'Blues',
-        colorbar_title = "Deaths"
-        ))
-        choropleth_map.update_layout(
-            title_text = 'Police Shooting Deaths by US States',
-            geo_scope='usa'
-        )
-        
-    if stackBarClick:
-        gender_id = stackBarClick['points'][0]['curveNumber']
-        if gender_id==0:
-            gender='Female'
-        else:
-            gender='Male'
-            
-        age = stackBarClick['points'][0]['x']
+        choropleth_map = create_choropleth_map(filter_by_race)
+        viz_states['pie_chart'] = 1
+    elif triggered_element =='stacked-bar-chart':
+        gender, age= get_age_and_gender(stackBarClick)
         filter_by_age_gender = df[(df['gender']==gender) & (df['age_bins']==age)]
-        df_group_by_state = filter_by_age_gender.groupby('state')['name'].agg('count').reset_index().rename(columns={'name':'count'})
-        
-        choropleth_map = go.Figure(data=go.Choropleth(
-        locations=df_group_by_state['state'],
-        z = df_group_by_state['count'],
-        locationmode = 'USA-states',
-        colorscale = 'Blues',
-        colorbar_title = "Deaths"
-        ))
-        choropleth_map.update_layout(
-            title_text = 'Police Shooting Deaths by US States',
-            geo_scope='usa'
-        )
-        
-    if lineChartClick:
+        choropleth_map = create_choropleth_map(filter_by_age_gender)
+        viz_states['stacked_bar_chart'] = 1
+    elif triggered_element =='line-chart':
         startDate = lineChartClick['xaxis.range[0]']
         endDate = lineChartClick['xaxis.range[1]']
-    
         filter_by_date = df[(df['date']>=startDate) & (df['date']<=endDate)]
-        df_group_by_state = filter_by_date.groupby('state')['name'].agg('count').reset_index().rename(columns={'name':'count'})
-        
-        choropleth_map = go.Figure(data=go.Choropleth(
-                                   locations=df_group_by_state['state'],
-                                   z = df_group_by_state['count'],
-                                   locationmode = 'USA-states',
-                                   colorscale = 'Blues',
-                                   colorbar_title = "Deaths"
-                                   ))
-        choropleth_map.update_layout(
-            title_text = 'Police Shooting Deaths by US States',
-            geo_scope='usa'
-        )
-
-
-        
+        choropleth_map = create_choropleth_map(filter_by_date)
+        viz_states['line_chart'] = 1
     return choropleth_map
-        
+
+
 @app.callback(
     Output('pie-chart-interaction', 'figure'),
     [Input('choropleth-map', 'clickData'),
      Input('stacked-bar-chart', 'clickData'),
      Input('line-chart','relayoutData')], prevent_initial_call=True)
 def update_pie_chart(mapClick, stackBarClick, lineChartClick):
-    print('-------------------------------------')
-    print("Inside update pie chart function")
-    print("Map click:",mapClick)
-    print("Stacked Bar Click:",stackBarClick)
-    print("Line chart click:",lineChartClick)
-    if mapClick:
+    if viz_states['pie_chart']==1:
+        return dash.no_update
+    triggered_element = dash.callback_context.triggered[0]['prop_id'].split('.')[0]
+    if triggered_element == 'choropleth-map':
         filter_by_location = df[df['state']==mapClick['points'][0]['location']]
-        df_group_by_race = filter_by_location.groupby('race')['name'].agg('count').reset_index().rename(columns={'name':'count'})
-        pie_chart = go.Figure(data=[go.Pie(labels=df_group_by_race['race'], values=df_group_by_race['count'], title='Ethnicity Versus Killings', textinfo='label+percent',
-                                 insidetextorientation='radial')])
-    
-    if stackBarClick:
-        gender_id = stackBarClick['points'][0]['curveNumber']
-        if gender_id==0:
-            gender='Female'
-        else:
-            gender='Male'
-            
-        age = stackBarClick['points'][0]['x']
+        pie_chart = create_pie_chart(filter_by_location)
+        viz_states['choropleth_map'] = 1
+    if triggered_element =='stacked-bar-chart':
+        gender, age= get_age_and_gender(stackBarClick)
         filter_by_age_gender = df[(df['gender']==gender) & (df['age_bins']==age)]
-        df_group_by_race = filter_by_age_gender.groupby('race')['name'].agg('count').reset_index().rename(columns={'name':'count'})
-        
-        pie_chart = go.Figure(data=[go.Pie(labels=df_group_by_race['race'], values=df_group_by_race['count'], title='Ethnicity Versus Killings', textinfo='label+percent',
-                                 insidetextorientation='radial')])
-        
-    if lineChartClick:
-  
+        pie_chart = create_pie_chart(filter_by_age_gender)
+        viz_states['stacked_bar_chart'] = 1
+    if triggered_element =='line-chart':
         startDate = lineChartClick['xaxis.range[0]']
         endDate = lineChartClick['xaxis.range[1]']
-    
         filter_by_date = df[(df['date']>=startDate) & (df['date']<=endDate)]
-        df_group_by_race = filter_by_date.groupby('race')['name'].agg('count').reset_index().rename(columns={'name':'count'})
-    
-        pie_chart = go.Figure(data=[go.Pie(labels=df_group_by_race['race'], values=df_group_by_race['count'], title='Ethnicity Versus Killings', textinfo='label+percent',
-                                        insidetextorientation='radial')])
-        
-       
-            
+        pie_chart = create_pie_chart(filter_by_date)
+        viz_states['line_chart'] = 1
     return pie_chart
 
 @app.callback(
@@ -235,37 +166,24 @@ def update_pie_chart(mapClick, stackBarClick, lineChartClick):
     [Input('choropleth-map', 'clickData'),
      Input('pie-chart-interaction', 'clickData'),
      Input('line-chart','relayoutData')], prevent_initial_call=True)
-def update_bar_chart(mapClick, pieClick, lineChartClick):
-    print('-------------------------------------')
-    print("Inside update bar chart function")
-    print("Map click:",mapClick)
-    print("Pie click:",pieClick)
-    print("Line chart click:",lineChartClick)
-    
-    if mapClick:
+def update_stacked_bar_chart(mapClick, pieClick, lineChartClick):
+    if viz_states['stacked_bar_chart']==1:
+        return dash.no_update
+    triggered_element = dash.callback_context.triggered[0]['prop_id'].split('.')[0]
+    if triggered_element == 'choropleth-map':
         filter_by_location = df[df['state']==mapClick['points'][0]['location']]
-        df_group_by_age_gender = filter_by_location.groupby(['age_bins','gender'])['name'].agg('count').reset_index().rename(columns={'name':'count'})
-        stacked_bar = px.bar(df_group_by_age_gender, x="age_bins", y="count", color="gender",
-                            hover_data=['count'], barmode = 'stack', custom_data=['gender'])
-
-    if pieClick:
+        stacked_bar = create_stacked_bar_chart(filter_by_location)
+        viz_states['choropleth_map'] = 1
+    if triggered_element =='pie-chart-interaction':
         filter_by_race = df[df['race']==str(pieClick['points'][0]['label'])]
-        df_group_by_age_gender = filter_by_race.groupby(['age_bins','gender'])['name'].agg('count').reset_index().rename(columns={'name':'count'})
-        
-        stacked_bar = px.bar(df_group_by_age_gender, x="age_bins", y="count", color="gender",
-                            hover_data=['count'], barmode = 'stack' , custom_data=['gender'])
-        
-    if lineChartClick:     
+        stacked_bar = create_stacked_bar_chart(filter_by_race)
+        viz_states['pie_chart'] = 1
+    if triggered_element == 'line-chart':
         startDate = lineChartClick['xaxis.range[0]']
         endDate = lineChartClick['xaxis.range[1]']
-    
         filter_by_date = df[(df['date']>=startDate) & (df['date']<=endDate)]
-        df_group_by_age_gender = filter_by_date.groupby(['age_bins','gender'])['name'].agg('count').reset_index().rename(columns={'name':'count'})
-    
-        stacked_bar = px.bar(df_group_by_age_gender, x="age_bins", y="count", color="gender",
-                            hover_data=['count'], barmode = 'stack' , custom_data=['gender'])
-      
-                
+        stacked_bar = create_stacked_bar_chart(filter_by_date)
+        viz_states['stacked_bar_chart'] = 1
     return stacked_bar
 
 @app.callback(
@@ -274,41 +192,26 @@ def update_bar_chart(mapClick, pieClick, lineChartClick):
      Input('pie-chart-interaction', 'clickData'),
      Input('stacked-bar-chart','clickData')], prevent_initial_call=True)
 def update_line_chart(mapClick, pieClick, stackBarClick):
-    print('-------------------------------------')
-    print("Inside update line chart function")
-    print("Map click:",mapClick)
-    print("Pie click:",pieClick)
-    print("Stacked Bar Click:",stackBarClick)
-    
-    if mapClick:
+    if viz_states['line_chart']==1:
+        return dash.no_update
+    triggered_element = dash.callback_context.triggered[0]['prop_id'].split('.')[0]
+    if triggered_element == 'choropleth-map':
         filter_by_location = df[df['state']==mapClick['points'][0]['location']]
-        df_group_by_date = filter_by_location.groupby('date')['name'].agg('count').reset_index().rename(columns={'name':'count'})
-        line_chart = px.line(df_group_by_date, x='date', y="count")
-       
-
-    if pieClick:
+        line_chart = create_line_chart(filter_by_location)
+        viz_states['choropleth_map'] = 1
+    if triggered_element =='pie-chart-interaction':
         filter_by_race = df[df['race']==str(pieClick['points'][0]['label'])]
-        df_group_by_date = filter_by_race.groupby('date')['name'].agg('count').reset_index().rename(columns={'name':'count'})
-        line_chart = px.line(df_group_by_date, x='date', y="count")
-        
-    if stackBarClick:
-        gender_id = stackBarClick['points'][0]['curveNumber']
-        if gender_id==0:
-            gender='Female'
-        else:
-            gender='Male'
-            
-        age = stackBarClick['points'][0]['x']
+        line_chart = create_line_chart(filter_by_race)
+        viz_states['pie_chart'] = 1
+    if triggered_element =='stacked-bar-chart':
+        gender, age= get_age_and_gender(stackBarClick)
         filter_by_age_gender = df[(df['gender']==gender) & (df['age_bins']==age)]
-        df_group_by_date = filter_by_age_gender.groupby('date')['name'].agg('count').reset_index().rename(columns={'name':'count'})
-        line_chart = px.line(df_group_by_date, x='date', y="count")
-        
+        line_chart = create_line_chart(filter_by_age_gender)
+        viz_states['stacked_bar_chart'] = 1
     return line_chart
 
 #-----------------------------------------------------------------------------------------------------------------------------------
 
 if __name__ == '__main__':
     #app.run_server(debug=True)
-    app.run_server(debug=True, dev_tools_ui=False)
-
-    
+    app.run_server(debug=True)
